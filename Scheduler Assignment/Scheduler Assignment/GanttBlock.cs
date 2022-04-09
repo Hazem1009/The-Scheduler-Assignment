cf75effffa1b5a24d7bb899c6f204b82612c06f7
MMostafa-Hub <mmostafa.ibra21@gmail.com>
2022-04-09 23:35:08 +0200

    Added a Gantt Block Class

---
+++ Scheduler Assignment/Scheduler Assignment/GanttBlock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduler_Assignment
+{
+    internal class GanttBlock
+    {
+        string name { get; set; }
+        int startTime { get; set; }
+        int endTime { get; set; }
+
+        GanttBlock(string name, int startTime, int endTime)
+        {
+            this.name = name;
+            this.startTime = startTime;
+            this.endTime = endTime;
+        }
+    }
+
+}
