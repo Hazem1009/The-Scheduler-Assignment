07c53291d0cff963e13802310a5f26f20c330bbd
MMostafa-Hub <mmostafa.ibra21@gmail.com>
2022-04-12 08:32:14 +0200

    Fixed Processes with the same Priority bug

--- Scheduler Assignment/Scheduler Assignment/PriorityPreemptive.cs
+++ Scheduler Assignment/Scheduler Assignment/PriorityPreemptive.cs
@@ -16,73 +16,83 @@ namespace Scheduler_Assignment
         {
             float totaWaitingTime = 0, totaTurnAroundTime = 0;
             List<GanttBlock> ganttBlocks = new List<GanttBlock>();
-            /*A list of all Arrival times To keep track of our check points in time*/
+
+            /* A list of all Arrival times To keep track of our check points in time */
             List<float> checkTimes = new List<float>();
             foreach (Process process in processes)
             {
                 checkTimes.Add(process.arrivalTime);
             }
-            /*Sorting the check points*/
+
+            /* Sorting the check points */
             checkTimes.Sort();
-            /*To remove Duplicate check times*/
-            checkTimes=checkTimes.Distinct().ToList();
-            /*a priority queue to add already arrived items to keep track of processing of next and interrupted processes,prio is according to Process priority*/
-            PriorityQueue<Process, int> pq = new PriorityQueue<Process, int>();
-            /*looping on all check times*/
+
+            /* To remove Duplicate check times */
+            checkTimes = checkTimes.Distinct().ToList();
+
+
+            /* a List to add already arrived items to keep track of processing of next and interrupted processes, prio is according to Process Priority and then the Arrival time */
+            List<Process> sortedProcesses = new List<Process>();
+
+            /* looping on all check times */
             for (int i = 0; i < checkTimes.Count; i++)
             {
-                /*each process started in this check time is queued to our priority queue*/
+                /* each process started in this check time is Added the list */
                 foreach (Process process in processes)
                 {
                     if (process.arrivalTime == checkTimes[i])
-                        pq.Enqueue(process, process.priority.Value);
+                    {
+                        sortedProcesses.Add(process);
+                    }
                 }
-                /*We dont need to continue with same logic if its the last check point we can now start dequeuing based on prio only*/
+
+                /* Sorting the processes based on Priority then based on the Arrival Time */
+                sortedProcesses = sortedProcesses.OrderBy(p => p.priority).ThenBy(p => p.arrivalTime).ToList();
+
+                /* We dont need to continue with same logic if its the last check point we can now start dequeuing directly */
                 if (i == checkTimes.Count - 1)
                     break;
 
-                Process p_max_priority = pq.Dequeue();
-                /*Condition to insert a new check point if current process can be completely finished in time less than difference between 2 check times*/
-                if (p_max_priority.remainingTime < checkTimes[i + 1] - checkTimes[i])
+                /* Dequeing from the list */
+                Process pMaxPriority = sortedProcesses[0];
+                sortedProcesses.RemoveAt(0);
+
+                /* Condition to insert a new check point if current process can be completely finished in time less than difference between 2 check times */
+                if (pMaxPriority.remainingTime < checkTimes[i + 1] - checkTimes[i])
                 {
-                    checkTimes.Insert(i + 1, p_max_priority.remainingTime - checkTimes[i]);
+                    checkTimes.Insert(i + 1, pMaxPriority.remainingTime - checkTimes[i]);
                 }
-                p_max_priority.remainingTime = p_max_priority.remainingTime - (checkTimes[i + 1] - checkTimes[i]);
-                /*We need to requeue the process if still got remaining time*/
-                if (p_max_priority.remainingTime > 0)
+                pMaxPriority.remainingTime = pMaxPriority.remainingTime - (checkTimes[i + 1] - checkTimes[i]);
+
+                /* We need to requeue the process if still got remaining time */
+                if (pMaxPriority.remainingTime > 0)
                 {
-                    pq.Enqueue(p_max_priority, p_max_priority.priority.Value);
+                    sortedProcesses.Add(pMaxPriority);
                 }
-                /*Creating a gantt block of processed process*/
-                ganttBlocks.Add(new GanttBlock(p_max_priority.name, checkTimes[i], checkTimes[i + 1]));
+
+                /* Creating a gantt block of processed process */
+                ganttBlocks.Add(new GanttBlock(pMaxPriority.name, checkTimes[i], checkTimes[i + 1]));
             }
-            /*Now we have passed all check points in Time(check times) and accordingly we just keep dequeuing and processing according to prio*/
+
+            /* Now we have passed all check points in Time(check times) and accordingly we just keep dequeuing and processing according to prio */
             float time = checkTimes[checkTimes.Count - 1];
-            while (pq.Count > 0)
+            while (sortedProcesses.Count > 0)
             {
-                Process p = pq.Dequeue();
-                  /*Handles a problem if 2 processes have same prio but p2 has higher process number than 1 
-                    *p2 arrived earlier than p1 but both have same prio p1 would interrupt p2 without following block
-                    */
-                if ((pq.Count!=0)&&(pq.Peek().priority.Value == p.priority.Value)) { 
-                    
-                    if (pq.Peek().arrivalTime < p.arrivalTime)
-                    {
-                        Process holder = pq.Dequeue();
-                        pq.Enqueue(p,p.priority.Value);
-                        p=holder;
-                    }
-                }
-                     
+                /* Dequing from the list */
+                Process p = sortedProcesses[0];
+                sortedProcesses.RemoveAt(0);
+
                 ganttBlocks.Add(new GanttBlock(p.name, time, time + p.remainingTime));
 
-                time = time + p.remainingTime;/*To keep up with our timeline*/
-                p.remainingTime = 0;/*Setting Remaining time of process to 0 as finished*/
+                time = time + p.remainingTime;/* To keep up with our timeline */
+                p.remainingTime = 0;/* Setting Remaining time of process to 0 as finished */
             }
-            /*Calculation for waiting time and turn around time*/
+
+
+            /* Calculation for waiting time and turn around time */
             foreach (var process in processes)
             {
-                /*Finds a list of all gantt blocks containing process name to find start and departure time*/
+                /* Finds a list of all gantt blocks containing process name to find start and departure time */
                 var matchedList = ganttBlocks.FindAll(delegate (GanttBlock p)
                 {
                     return p.name == process.name;
@@ -91,19 +101,21 @@ namespace Scheduler_Assignment
                 totaTurnAroundTime += matchedList[matchedList.Count - 1].endTime - process.arrivalTime;
                 totaWaitingTime += matchedList[matchedList.Count - 1].endTime - process.arrivalTime - process.burstTime;
             }
+
+            /* Merge adjacent Gantt blocks with the same name */
             List<GanttBlock> toDrawBlocks = new List<GanttBlock>();
-            for (int it = 0; it < ganttBlocks.Count ; it++)
+            for (int i = 0; i < ganttBlocks.Count; i++)
             {
-                float start = ganttBlocks[it].startTime;
-                float end = ganttBlocks[it].endTime;
-                while ((it < ganttBlocks.Count - 1) && (ganttBlocks[it + 1].name == ganttBlocks[it].name))
+                float start = ganttBlocks[i].startTime;
+                float end = ganttBlocks[i].endTime;
+                while ((i < ganttBlocks.Count - 1) && (ganttBlocks[i + 1].name == ganttBlocks[i].name))
                 {
-                    it++;
-                    end = ganttBlocks[it].endTime;
+                    i++;
+                    end = ganttBlocks[i].endTime;
                 }
-                toDrawBlocks.Add(new GanttBlock(ganttBlocks[it].name, start, end));
+                toDrawBlocks.Add(new GanttBlock(ganttBlocks[i].name, start, end));
             }
-           
+
             return (totaWaitingTime / processes.Count, totaTurnAroundTime / processes.Count, toDrawBlocks);
         }
     }
