0aad67307002d5dfdf76295cc454502575b17391
Mohanad <mohanadismail789@gmail.com>
2022-04-16 12:50:31 +0200

    Close #4
    Disable deletion, addition or resizing columns and rows

--- Scheduler Assignment/Scheduler Assignment/RRDataWindow.Designer.cs
+++ Scheduler Assignment/Scheduler Assignment/RRDataWindow.Designer.cs
@@ -95,6 +95,10 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.PName,
@@ -102,6 +106,7 @@
             this.Burst});
             this.dataGridView1.Location = new System.Drawing.Point(488, 103);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 29;
             this.dataGridView1.Size = new System.Drawing.Size(300, 188);
